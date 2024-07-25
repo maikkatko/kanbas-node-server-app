808d6bbfe1c8640806054e99fa05be287478c004
maikkatko <maikkatko@gmail.com>
2024-07-24 20:38:09 -0400

    Publish branch

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import "dotenv/config";
 import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
 import cors from "cors";
@@ -6,9 +9,13 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
