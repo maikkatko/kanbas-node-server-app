6f5c671492dd29a138885c39f02b50c63115f3b9
maikkatko <maikkatko@gmail.com>
2024-07-31 13:29:13 -0400

    update debug statements

--- App.js
+++ App.js
@@ -10,19 +10,46 @@ import AssignmentRoutes from './Assignments/routes.js';
 import Hello from './hello.js';
 import Lab5 from './Lab5/index.js';
 
-import model from "./Users/model.js";
+import usersModel from "./Users/model.js";
+import coursesModel from "./Courses/model.js";
+import modulesModel from "./Modules/model.js";
+import assignmentsModel from "./Assignments/model.js";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 
 mongoose.connect(CONNECTION_STRING);
 
-const allData = await model.find({});
+const userData = await usersModel.find({});
 console.log('All data in the User database:');
-console.log(JSON.stringify(allData, null, 2));
+console.log(JSON.stringify(userData, null, 2));
 
 // Optional: You can also count the documents
-const count = await model.countDocuments();
-console.log(`Total number of documents: ${count}`);
+const userCount = await usersModel.countDocuments();
+console.log(`Total number of documents: ${userCount}`);
+
+const coursesData = await coursesModel.find({});
+console.log('All data in the User database:');
+console.log(JSON.stringify(coursesData, null, 2));
+
+// Optional: You can also count the documents
+const coursesCount = await coursesModel.countDocuments();
+console.log(`Total number of documents: ${coursesCount}`);
+
+const modulesData = await coursesModel.find({});
+console.log('All data in the User database:');
+console.log(JSON.stringify(modulesData, null, 2));
+
+// Optional: You can also count the documents
+const modulesCount = await coursesModel.countDocuments();
+console.log(`Total number of documents: ${modulesCount}`);
+
+const assignmentsData = await coursesModel.find({});
+console.log('All data in the User database:');
+console.log(JSON.stringify(assignmentsData, null, 2));
+
+// Optional: You can also count the documents
+const assignmentsCount = await coursesModel.countDocuments();
+console.log(`Total number of documents: ${assignmentsCount}`);
 
 const app = express()
 
