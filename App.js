61b8c6e49834ed0879688170468241b6f91069c8
maikkatko <maikkatko@gmail.com>
2024-07-31 13:12:38 -0400

    Add additional debug statements

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import express from 'express';
 import mongoose from "mongoose";
 import session from "express-session";
-import UserRoutes from "./Users/routes.js";
-import "dotenv/config";
-import Hello from './hello.js';
-import Lab5 from './Lab5/index.js';
 import cors from "cors";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
 import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from './Modules/routes.js';
 import AssignmentRoutes from './Assignments/routes.js';
+import Hello from './hello.js';
+import Lab5 from './Lab5/index.js';
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-console.log(CONNECTION_STRING);
-mongoose.connect(CONNECTION_STRING);
+
+try {
+  mongoose.connect(CONNECTION_STRING);
+
+  console.log('Successfully connected to MongoDB using Mongoose');
+
+  // Optional: You can perform additional checks here
+  // For example, you can check the connection state
+  console.log(`Connection state: ${mongoose.connection.readyState}`);
+  // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+} catch (error) {
+  console.error('Error connecting to MongoDB:', error);
+}
 
 const app = express()
+
 app.use(
   cors({
     credentials: true,
@@ -27,6 +39,7 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -35,10 +48,10 @@ if (process.env.NODE_ENV !== "development") {
     domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
+
 app.use(session(sessionOptions));
-
-
 app.use(express.json());
+
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
